f557c3392c2d91c3e64ad9f522870836b8f6da50
Luis Mesajil <lhmesajil@gmail.com>
2024-09-19 18:50:30 -0500

    feat(popup): Add getCurrentBgColor

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
-import { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import './App.css'
 import Footer from './popup/Footer/Footer'
+import { black, hexToRgb, rgbToHex } from './utils/utils'
 
 function App() {
-  const [color, setColor] = useState<string>('#000')
+  const [color, setColor] = useState<string>(black)
 
   // Function to update background color
   const changeBgColor = async (newColor: string) => {
     let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
-    if (tab.id) {
-      console.log({ 'tab.id': tab.id })
+    if (tab?.id) {
       chrome.scripting.executeScript({
         target: { tabId: tab.id },
         args: [newColor],
@@ -20,11 +20,33 @@ function App() {
     }
   }
 
+  // Function to get the current background color of the page
+  const getCurrentBgColor = async () => {
+    let [tab] = await chrome.tabs.query({ active: true, currentWindow: true })
+    if (tab?.id) {
+      const [{ result: bgColor }] = await chrome.scripting.executeScript({
+        target: { tabId: tab.id },
+        func: () => {
+          const bgColor = window.getComputedStyle(document.body).backgroundColor
+          return bgColor
+        },
+      })
+
+      // Convert RGB color to Hex
+      setColor(rgbToHex(bgColor) || black)
+    }
+  }
+
+  // UseEffect to get the background color when the component mounts
+  useEffect(() => {
+    getCurrentBgColor()
+  }, [])
+
   // Handler for color change event
   const handleColorChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const newColor: string = e.currentTarget.value
-    setColor(newColor)
-    changeBgColor(newColor)
+    const hexColor: string = e.currentTarget.value
+    setColor(hexColor)
+    changeBgColor(hexColor)
   }
 
   // Popup
@@ -33,11 +55,11 @@ function App() {
       <div></div>
       <h1>Update Background Color</h1>
       <div className='card'>
-        <h2>Selected color: {color}</h2>
+        <h2>{hexToRgb(color).toUpperCase()}</h2>
         <div>
-          <input type='color' className='color-circle' onChange={handleColorChange} />
+          <input type='color' className='color-circle' onChange={handleColorChange} value={color} />
         </div>
-        <Footer></Footer>
+        <Footer />
       </div>
     </>
   )
---
+++ src/utils/utils.ts
@@ -0,0 +1,25 @@
+const black = '#000000'
+
+// Convert RGB color to Hexadecimal
+const rgbToHex = (rgb: string | undefined) => {
+  const result = rgb?.match(/\d+/g)
+  if (result && result.length >= 3) {
+    const r = parseInt(result[0]).toString(16).padStart(2, '0')
+    const g = parseInt(result[1]).toString(16).padStart(2, '0')
+    const b = parseInt(result[2]).toString(16).padStart(2, '0')
+    return `#${r}${g}${b}`
+  }
+  return black // Default to black if parsing fails
+}
+
+// Convert Hex color to RGB
+const hexToRgb = (hex: string): string => {
+  const normalizedHex = hex.replace('#', '')
+  const bigint = parseInt(normalizedHex, 16)
+  const r = (bigint >> 16) & 255
+  const g = (bigint >> 8) & 255
+  const b = bigint & 255
+  return `rgb(${r}, ${g}, ${b})`
+}
+
+export { black, rgbToHex, hexToRgb }
